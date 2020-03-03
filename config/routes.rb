2eda2bb6e11a248ef65657cd67526db002cbb7b4
svenia wurm <svwu@outlook.com>
2020-03-03 14:35:34 +0100

    fixed the bug with the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'self_ratings/new'
+  get 'self_ratings/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
