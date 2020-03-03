fbf7b0795d29bfa005af9682a76afef8da08efa4
Matthijs Kralt <matthijs29@live.com>
2020-03-03 11:58:15 +0100

    Add path for cars new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'cars/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :users do
+    resources :cars, only: [:new, :create]
+  end
 end
