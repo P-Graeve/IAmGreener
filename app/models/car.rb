c0e853bfc7333bae613fca18c04fc5178188370e
Matthijs Kralt <matthijs29@live.com>
2020-03-03 11:49:57 +0100

    Play with car api

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,19 @@
+require 'open-uri'
+
 class Car < ApplicationRecord
   belongs_to :user
 
   # brand
   # model
 
-
   def fetch_info
-    model
+    test_brand = 'Hyundai'
+    test_model = 'IX35'
+    test_year = 2011
+    json = open("https://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=#{test_brand}&year=#{test_year}").read;
+    data = JSON.parse(json)["Trims"]
+    data.select do |model|
+      model["model_name"] == test_model.downcase
+    end
   end
 end
