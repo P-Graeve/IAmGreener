c01fd6ae4a17376c661f12c71ffdb6a372d382cd
Matthijs Kralt <matthijs29@live.com>
2020-03-03 16:04:22 +0100

    Cancel select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,6 @@ import "bootstrap";
 
 import { initHamburgerIcon } from '../nav_menu/navMenu';
 import { initNewCarForm } from '../new_car/newCarForm';
-import { initSelect2 } from '../plugins/select2';
 
 initNewCarForm();
 initHamburgerIcon();
-initSelect2();
--- app/javascript/plugins/select2.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-import 'select2';
-
-export const initSelect2 = () => {
-  console.log($('select2'));
-  $('.select2').select2();
-}
