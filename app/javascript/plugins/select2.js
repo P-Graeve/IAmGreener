7f3fc4f3d38233dd2d6e7958df5b591451a043f3
Matthijs Kralt <matthijs29@live.com>
2020-03-03 15:18:16 +0100

    Add functionality in select2.js

---
+++ app/javascript/plugins/select2.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import select2 from 'select2';
+
+export const initSelect2 = () => {
+  const select2Elems = document.querySelectorAll('.select2');
+  select2Elems.forEach(elem => {
+
+  })
+}
