581a66eac68558b698f91d2bad35559ccaae79af
Matthijs Kralt <matthijs29@live.com>
2020-03-03 16:33:26 +0100

    add 2 inputs for the car brand and year, add fetchBrand function

--- app/javascript/new_car/newCarForm.js
+++ app/javascript/new_car/newCarForm.js
@@ -1,3 +1,25 @@
+const fetchBrand = () => {
+  // check if the model year is already open or not
+  const yearSelect = document.querySelector('#year-select');
+  if (yearSelect.classList.contains('none')) {
+    // remove none class
+    yearSelect.classList.remove('none');
+  } else {
+    // it doesnt have none class, which means we can send the fetchbrands so we can get a model
+    const brand = document.querySelector('#brand-select');
+    const year = yearSelect.value;
+  }
+}
+
+const addListeners = () => {
+  // add listener to the yes button on the form
+  document.querySelector('#yes-btn').addEventListener('click', nextQuestion);
+  // add listener to the form of the car brand
+  document.querySelectorAll('.car-info-input').forEach(input => {
+    input.addEventListener('change', fetchBrand);
+  });
+}
+
 const nextQuestion = () => {
   // get the current question order
   const order = parseInt(document.querySelector('.selected').dataset.order);
@@ -14,8 +36,8 @@ const nextQuestion = () => {
 }
 
 export const initNewCarForm = () => {
-  // add listener to the yes button on the form
-  document.querySelector('#yes-btn').addEventListener('click', nextQuestion);
+  addListeners();
+
   const questionSections = document.querySelectorAll('.car-question');
   if (questionSections) {
     questionSections.forEach(section => {
