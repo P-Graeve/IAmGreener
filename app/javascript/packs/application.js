153a3b8c8c84e09317158a8dc9e0fba5e23bbd00
Matthijs Kralt <matthijs29@live.com>
2020-03-03 15:14:38 +0100

    Init select 2 in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import "bootstrap";
 
 import { initHamburgerIcon } from '../nav_menu/navMenu';
 import { initNewCarForm } from '../new_car/newCarForm';
+import { initSelect2 } from '../plugins/select2';
 
 initNewCarForm();
 initHamburgerIcon();
+initSelect2();
