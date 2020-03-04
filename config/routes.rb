1508bc26f02b18698b30e216e8dd5d642fd0b0b7
svenia wurm <svwu@outlook.com>
2020-03-04 14:53:44 +0100

    created route, controlleracttion, and view for the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'self_ratings/new'
   post 'self_ratings/create'
+  get 'dashboard', to: 'pages#dashboard'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
