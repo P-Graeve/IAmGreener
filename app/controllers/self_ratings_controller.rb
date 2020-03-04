ef89ba10a2c34fc03a97dd74ad9455a9e107d08d
svenia wurm <svwu@outlook.com>
2020-03-04 12:02:08 +0100

    add some sublime javascript

--- app/controllers/self_ratings_controller.rb
+++ app/controllers/self_ratings_controller.rb
@@ -7,11 +7,18 @@ class Users::SelfRatingsController < ApplicationController
   end
 
   def create
-    @self_rating = SelfRating.new(self_rating[params[:id]])
+    raise
+    @self_rating = SelfRating.new(self_rating_params)
     if @self_rating.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  private
+
+  def self_rating_params
+    params.require(:self_rating).permit(:pickup_trash_score, :conscious_score, :overall_score);
   end
 end
