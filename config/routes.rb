d852ad842a3a4ce32da529d2c03451e397f5570a
Matthijs Kralt <matthijs29@live.com>
2020-03-04 16:52:18 +0100

    Add custom controller for registrations with custom redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'self_ratings/new'
   post 'self_ratings/create'
   get 'dashboard', to: 'pages#dashboard'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
