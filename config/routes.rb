df575217fe3381931e9d45562595a8d88312f460
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-04 13:31:54 +0100

    add profile route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
     resources :cars, only: [:new, :create]
+    get "profile",  to: 'pages#profile', as: :profile
   end
 end
