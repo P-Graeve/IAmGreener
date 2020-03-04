4022065b399a23b05b57aae04d4258dff5e1e2d0
Matthijs Kralt <matthijs29@live.com>
2020-03-04 17:27:02 +0100

    Change redirection after creating a new car

--- app/controllers/users/cars_controller.rb
+++ app/controllers/users/cars_controller.rb
@@ -31,7 +31,7 @@ class Users::CarsController < ApplicationController
 
         respond_to do |format|
           # redirect to dashboard page
-          format.html { redirect_to root_path }
+          format.html { redirect_to self_ratings_new_path }
           # redirect to dashboard page
           format.js { render :create_car }
         end
