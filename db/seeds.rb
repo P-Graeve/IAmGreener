0d75a2c01f0d479511d31cf8689cfb5d21fb3dc3
Matthijs Kralt <matthijs29@live.com>
2020-03-04 16:05:52 +0100

    Add tree score to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,20 @@
 require "open-uri"
 
 puts 'Cleaning DB...'
-
+Notificaton.destroy_all
 Car.destroy_all
 User.destroy_all
 
-user = User.new(username: "Frans", email: "frans@example.com" , password: "Password", password_confirmation: 'Password')
+user = User.new(username: "Frans", email: "frans@example.com" , password: "Password", password_confirmation: 'Password', tree_score: 453)
 img = open('https://images.unsplash.com/photo-1472711795975-42c5b4ee828c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80')
 user.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
 user.save
 
-puts "Success! You have #{User.count} users"
+# generate some fake notifications
+4.times do
+  notif = Notificaton.new(message: Faker::Hacker.say_something_smart)
+  notif.user = user
+  notif.save
+end
+
+puts "Success! You have #{User.count} users, #{Notificaton.count} notifications"
