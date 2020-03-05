6ef1785262898e84fcb2d9774250b2dd5be89d09
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-05 18:00:57 +0100

    route challenge and tip

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'challenges/show'
+  get 'tips/show'
   get 'dashboard', to: 'pages#dashboard'
   devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   resources :categories, only: [:show]
+  resources :challenges, only: [:show]
+  resources :tips, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
     resources :cars, only: [:new, :create]
