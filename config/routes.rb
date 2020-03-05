5cad1fec2b2875712dd5eebdfd264c808eb1e9c9
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-05 11:22:38 +0100

    category route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'pages#dashboard'
   devise_for :users
   root to: 'pages#home'
+  resources :categories, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
     resources :cars, only: [:new, :create]
