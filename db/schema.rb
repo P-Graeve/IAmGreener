3d93f8ee748fb2341b9d8d66073e51dd09d67549
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-05 16:23:06 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_114553) do
+ActiveRecord::Schema.define(version: 2020_03_05_140244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_114553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "image_url"
     t.index ["category_id"], name: "index_challenges_on_category_id"
   end
 
@@ -117,6 +118,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_114553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "image_url"
     t.index ["category_id"], name: "index_tips_on_category_id"
   end
 
