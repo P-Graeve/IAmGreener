4fb5775ed6e5dee2385c0a1fb37f7d0dc5a9d985
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-06 15:18:43 +0100

    add update action for dailyprogress and redirect to right page

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,5 +1,6 @@
 class ChallengesController < ApplicationController
   def show
     @challenge = Challenge.find(params[:id])
+    @daily_progress = current_user.todays_progress
   end
 end
--- app/controllers/daily_progresses_controller.rb
+++ app/controllers/daily_progresses_controller.rb
@@ -1,21 +1,24 @@
 class DailyProgressesController < ApplicationController
 
   def create
+    raise
+  end
+
+  def update
     @challenge = Challenge.find(params[:challenge_id])
     @daily_progress = current_user.todays_progress
-    if params_choice == 'completed'
+    if params_choice[:daily_challenge_completed] == 'completed'
       # update daily progress with true for challenge completed
       tree_count = 35
       # update the instance of todays progress
-      @daily_progress.update(tree_amount: tree_count)
+      @daily_progress.update(tree_amount: tree_count, daily_challenge_completed: true)
       # update the user's tree count
-      current_user.update(tree_count: current_user.tree_count + tree_count)
-
-      redirect_to 'daily_progresses/completed'
+      current_user.update(tree_score: current_user.tree_score + tree_count)
+      redirect_to daily_progresses_completed_path
     else
-      redirect_to 'daily_progresses/failed'
+      @daily_progress.update(daily_challenge_completed: false)
+      redirect_to daily_progresses_failed_path
     end
-
   end
 
   def completed
