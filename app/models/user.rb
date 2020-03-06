855bcf73b473d89ec2bed3288492e7eb45871402
Matthijs Kralt <matthijs29@live.com>
2020-03-06 12:10:23 +0100

    Add method todays_progress to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,17 @@ class User < ApplicationRecord
       progress.tree_amount
     end
   end
+
+  def todays_progress
+    # return the last instance of daily_progresses for today
+    # check if it is already made
+    pg = DailyProgress.find_by(date: 0.days.ago.strftime('%d-%m-%y'))
+    if pg
+      # return this object
+      pg
+    else
+      # return a new instance of dailyprogress
+      DailyProgress.new(user: self)
+    end
+  end
 end
