ffc15099868af59eba55a2803b1889c29526b58b
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-10 14:42:57 +0100

    add destroy

--- app/controllers/users/notifications_controller.rb
+++ app/controllers/users/notifications_controller.rb
@@ -1,5 +1,11 @@
 class Users::NotificationsController < ApplicationController
   def index
-    @notifications = Notification.where(user: current_user)
+    @notifications = Notification.order('updated_at DESC').where(user: current_user)
+  end
+
+  def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+    redirect_to users_notifications_path
   end
 end
