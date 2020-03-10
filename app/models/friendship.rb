0282c54ccd0a3c2636d046fe06d181ad9f8a9819
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-10 14:45:25 +0100

    update friend request

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,11 +6,19 @@ class Friendship < ApplicationRecord
 
   # hook on after_create -> we create a new notification
   after_create :send_new_notification
-
+  after_update :update_notification
 
   private
 
   def send_new_notification
-    Notification.create(user_id: friend_id, message: "#{user.username} sent you a request")
+    notification = Notification.new(user_id: friend_id, message: "#{user.username} sent you a request", friendship: self)
+    notification.friend_request!
+  end
+
+  def update_notification
+    notification = Notification.find_by(friendship: self)
+    notification.friend_accepted!
+    # create notification for the friend as well
+    notification = Notification.create(user: user, message: "Your request got accepted by #{friend.username}")
   end
 end
