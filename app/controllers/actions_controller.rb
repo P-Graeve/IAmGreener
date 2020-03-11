9cf24b21f6015ab3fc8f9d394f3a1eed9d9e825c
Matthijs Kralt <matthijs29@live.com>
2020-03-11 11:37:20 +0100

    Add action controller create action

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,4 +1,16 @@
 class ActionsController < ApplicationController
   def create
+    action = Action.new(action_params, count: 1)
+    if action.save
+      redirect_to dashboard_path
+    else
+      raise
+    end
+  end
+
+  private
+
+  def action_params
+    params.require(:action).permit(:name, :badge_id)
   end
 end
