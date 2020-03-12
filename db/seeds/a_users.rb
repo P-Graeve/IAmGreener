2218f604157a0dc05a2b493da03f595ad436f70a
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-12 14:58:55 +0100

    add some earn tree actions for the other users

--- db/seeds/a_users.rb
+++ db/seeds/a_users.rb
@@ -1,3 +1,4 @@
+# user4
 user4 = User.new(username: "John", email: "John@example.com" , password: "Password", password_confirmation: 'Password')
 img = open('https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')
 user4.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
@@ -14,6 +15,9 @@ bentley = Car.create(model: 'Flying Spur', brand: 'Bentley', year: 2015, user: u
 car_ac = Action.new(user: user4, car: bentley, count: 1)
 car_ac.add_car!
 
+Action.create(name: 'earn_tree', count: 345, user: user4)
+
+# user5
 user5 = User.new(username: "Lucy", email: "lucy@example.com" , password: "Password", password_confirmation: 'Password')
 img = open('https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')
 user5.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
@@ -30,10 +34,14 @@ bmw = Car.create(model: '6Series', brand: 'BMW', year: 2016, user: user5, mpg: 2
 car_ac = Action.new(user: user5, car: bmw, count: 1)
 car_ac.add_car!
 
+Action.create(name: 'earn_tree', count: 50, user: user5)
+
+# user7
 user7 = User.new(username: "Carla", email: "carla@example.com" , password: "Password", password_confirmation: 'Password')
 img = open('https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')
 user7.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
 user7.save
+
 
 # create sign up action
 sign_up_action = Action.new(user: user7, count: 1, created_at: 1.month.ago)
@@ -46,6 +54,9 @@ mini = Car.create(model: 'Cooper', brand: 'Mini', year: 2018, user: user7, mpg: 
 car_ac = Action.new(user: user7, car: mini, count: 1)
 car_ac.add_car!
 
+Action.create(name: 'earn_tree', count: 605, user: user7)
+
+# user 8
 user8 = User.new(username: "Sophie", email: "sopie@example.com" , password: "Password", password_confirmation: 'Password')
 img = open('https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')
 user8.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
@@ -62,6 +73,9 @@ ford = Car.create(model: 'Fiesta', brand: 'Ford', year: 2016, user: user8, mpg: 
 car_ac = Action.new(user: user8, car: ford, count: 1)
 car_ac.add_car!
 
+Action.create(name: 'earn_tree', count: 231, user: user8)
+
+# user9
 user9 = User.new(username: "Jack", email: "jack@example.com" , password: "Password", password_confirmation: 'Password')
 img = open('https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')
 user9.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
