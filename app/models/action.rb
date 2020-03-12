ef67cc744ea79c1f7cbb4a684d2554c9b5760824
Matthijs Kralt <matthijs29@live.com>
2020-03-12 11:08:27 +0100

    Complete check_for_badge

--- app/models/action.rb
+++ app/models/action.rb
@@ -37,11 +37,10 @@ class Action < ApplicationRecord
 
   def check_for_badge
     new_badge = Badge.generate_for(self)
-    # badge = Badge.generate_for(self)
-    # if badge && !user.badges.include?(badge)
-    #   # create new action -> earn badge
-    #   Action.create(user: user, count: 1, name: 'earn_badge', badge: badge)
-    # end
+    if badge && !user.all_badges.include?(badge)
+      # create a new action -> earn badge
+      Action.create(user: user, count: 1, name: 'earn_badge', badge: badge)
+    end
   end
 
   def timestamp_attributes_for_create
