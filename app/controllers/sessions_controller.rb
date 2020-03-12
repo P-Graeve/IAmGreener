02467ceeebaf899ddea4c636411fa937032ba8f4
Matthijs Kralt <matthijs29@live.com>
2020-03-12 16:29:55 +0100

    Fix redirect on signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,12 @@ class SessionsController < Devise::SessionsController
   def create
     super
     # create sign in action
-    action = Action.new(user: @user, count: 1)
-    action.sign_in!
+    action = Action.create(user: @user, count: 1, name: 'sign_in')
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    :dashboard # Or :prefix_to_your_route
   end
 end
