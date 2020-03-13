6a74cdd17a5b7c890a1463d9a74cc993bfe9495f
Matthijs Kralt <matthijs29@live.com>
2020-03-13 11:03:21 +0100

    add jad pic, change mpg generation

--- db/seeds/a_frans.rb
+++ db/seeds/a_frans.rb
@@ -1,13 +1,13 @@
 user7 = User.new(username: "Frans", email: "frans@example.com" , password: "Password", password_confirmation: 'Password')
-img = open('https://res-2.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/ggbjbwmdzcrtu9wwgf6u')
-user7.avatar.attach(io: img, filename: 'avatar.png', content_type: 'image/png')
+img = open('jad.jpg')
+user7.avatar.attach(io: img, filename: 'avatar.jpg', content_type: 'image/jpg')
 user7.save
 
 # create sign up action
 sign_up_action = Action.create(user: user7, count: 1, created_at: 1.month.ago, name: 'sign_up')
 # create car
 # generate a car
-audi = Car.create(model: 'A6', brand: 'Audi', year: 2014, user: user7, mpg: 26.3)
+audi = Car.create(model: 'A6', brand: 'Audi', year: 2014, user: user7, mpg: 35.2)
 # create car add action
 car_ac = Action.create(user: user7, car: audi, count: 1, name: 'add_car')
 
